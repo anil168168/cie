caedd359c8110d718797aafcb1763ef4ff82eabf
anil168168 <clggithub@gmail.com>
2025-04-04 11:09:48 +0530

    save file

---
+++ data.java
@@ -0,0 +1,19 @@
+public class DataTypesExample {
+
+    public static void main(String[] args) {
+        
+       int intVar = 25;
+       float floatVar = 23.55;
+        double doubleVar = 22.27;
+        char charVar = 'A';
+        boolean boolVar = true;
+        long longVar = 7090549024;
+        
+        System.out.println("int : " + intVar);
+        System.out.println("float : " + floatVar);
+        System.out.println("double : " + doubleVar);
+        System.out.println("char : " + charVar);
+        System.out.println
+        System.out.println("long : " + longVar);
+    }
+}
